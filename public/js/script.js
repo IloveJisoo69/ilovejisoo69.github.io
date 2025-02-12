114a8b0400ad827d0069841c185994beed9294bc
IloveJisoo69 <liamshadow2524@gmail.com>
2025-02-12 20:10:47 +0800

    Test

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,12 @@
 const answers_no = [
-    "No",
-    "Are you sure?",
-    "Are you really sure??",
-    "Are you really really sure???",
-    "Think again?",
-    "Don't believe in second chances?",
-    "Why are you being so cold?",
-    "Maybe we can talk about it?",
-    "I am not going to ask again!",
-    "Ok now this is hurting my feelings!",
-    "You are now just being mean!",
-    "Why are you doing this to me?",
-    "Please give me a chance!",
-    "I am begging you to stop!",
-    "Ok, Let's just start over.."
+    "No 😨",
+    "Are you sure? 🥺",
+    "Are you really sure?? 🥺🥺🥺",
+    "Are you really really sure??? 😭😭😭",
+    "Think again? 😀",
+    "Maybe we can talk about it? 🤠",
+    "Please give me a chance! 🥰",
+    "Ok, Let's just start over.. 💀"
 ];
 
 const answers_yes = "Yes";
